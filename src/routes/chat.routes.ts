1fcdfbfd893333cc022821ae33ae33c5ce8c2d07
CarlosSiqueira <carlooos.siqueira@gmail.com>
2025-10-27 16:40:04 -0300

    fix: multi part body middleware

--- src/routes/chat.routes.ts
+++ src/routes/chat.routes.ts
@@ -2,10 +2,11 @@ import { Router } from "express"
 import { chatController } from "../controllers"
 import { validateBody } from "../middlewares/body.validator.middleware"
 import { ChatDTOSchema } from "../schemas/chat.schema"
+import { multiPartBody } from "../middlewares/multi.part.middleware"
 
 const chat = Router()
 
 chat.post('/message', validateBody(ChatDTOSchema), chatController.createMessage)
-chat.post('/webhook-sendgrid', chatController.webhookSendgrid)
+chat.post('/webhook-sendgrid', ...multiPartBody, chatController.webhookSendgrid)
 
 export { chat }
