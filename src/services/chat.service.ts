4bbf317f78e090491313315ce720ef788fd06b53
CarlosSiqueira <carlooos.siqueira@gmail.com>
2025-10-27 11:45:42 -0300

    fix: url api webhook

--- src/services/chat.service.ts
+++ src/services/chat.service.ts
@@ -31,7 +31,7 @@ class ChatService implements IChat {
       const message = extractPlainTextFromMime(data.email)
 
       if (header.subdomain && header.codigoChat) {
-        const request = createApiClient('https://carlos-local.athenas.com.br/apix/', 'localhost')
+        const request = createApiClient(process.env.URLAPI || '', 'localhost')
 
         await request.post('Tarefas/Chat/message', {
           codigoChat: parseInt(header.codigoChat),
